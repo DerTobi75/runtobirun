e819a031ce563146f308f1a6ad159df4b616c6fb
Tobi <tobias@heyn.net>
2018-11-15 15:36:48 +0100

    Started Using twig and stuff

--- uploadfit.php
+++ uploadfit.php
@@ -1,3 +1,12 @@
 <?php
 
-echo "Das ist nur ein Test";
+require_once 'vendor/autoload.php';
+
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader, array(
+    'cache' => false,
+    'debug' => true,
+));
+$twig->addExtension(new Twig_Extension_Debug());
+
+echo $twig->render('layout.twig', array());
